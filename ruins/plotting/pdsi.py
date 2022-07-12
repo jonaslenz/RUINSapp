ddb123d9a5738e53cd979289a366adf7622a31d9
Mirko Mälicke <mirko.maelicke@kit.edu>
2022-07-12 14:27:49 +0200

    pdsi_plot handles subplots now

--- ruins/plotting/pdsi.py
+++ ruins/plotting/pdsi.py
@@ -2,12 +2,13 @@ from typing import List
 
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from ruins.core import Config, DataManager
 from ruins.processing.pdsi import multiindex_pdsi_data
 
 
-def pdsi_plot(dataManager: DataManager, config: Config, group_by: List[str] = None, filters: List[str] = None, colorscale: str = 'RdBu') -> go.Figure:
+def pdsi_plot(dataManager: DataManager, config: Config, group_by: List[str] = None, filters: List[str] = None, colorscale: str = 'RdBu', fig: go.Figure = None, row: int = 1, col: int = 1) -> go.Figure:
     """Heatmap plot for Palmer drought severity index"""
     # load the data
     pdsi = dataManager.read('pdsi')
@@ -20,7 +21,10 @@ def pdsi_plot(dataManager: DataManager, config: Config, group_by: List[str] = No
 
 
     # create the figure
-    fig = go.Figure(go.Heatmap(y=data.index, z=data.values, colorscale=colorscale))
+    if fig is None:
+        fig = make_subplots(rows=1, cols=1)
+
+    fig.add_trace(go.Heatmap(y=data.index, z=data.values, colorscale=colorscale), row=row, col=col)
     
     if group_by is not None:
         # extract the level
@@ -31,8 +35,8 @@ def pdsi_plot(dataManager: DataManager, config: Config, group_by: List[str] = No
 
         # add the annotations
         for l, u, lab in zip(positions[:-1], positions[1:], labels):
-            fig.add_annotation(x=u, ax=l, y=-.03, ay=-.03, xref='x', axref='x', yref='paper', arrowside='start+end', arrowhead=2, showarrow=True)
-            fig.add_annotation(x=int(l + (u - l) / 2), y=-.1, xref='x', yref='paper', text=lab.upper(), showarrow=False)
+            fig.add_annotation(x=u, ax=l, y=-.03, ay=-.03, xref='x', axref='x', yref='paper', arrowside='start+end', arrowhead=2, showarrow=True, row=row, col=col)
+            fig.add_annotation(x=int(l + (u - l) / 2), y=-.1, xref='x', yref='paper', text=lab.upper(), showarrow=False, row=row, col=col)
 
         # remove the x-axis
         fig.update_layout(
