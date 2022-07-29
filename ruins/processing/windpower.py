d85b6983ff4a791dd4eee80380000e119cb7bfe3
Mirko Mälicke <mirko.maelicke@kit.edu>
2022-07-29 10:38:41 +0200

    added transpated R-codes

--- ruins/processing/windpower.py
+++ ruins/processing/windpower.py
@@ -1,8 +1,11 @@
 from typing import Union, Tuple, List
 import warnings
+from itertools import product
+from collections import defaultdict
 
 import numpy as np
 import pandas as pd
+from scipy.stats import gaussian_kde
 
 from ruins.core import DataManager
 
@@ -203,3 +206,208 @@ def windpower_actions_projection(dataManager: DataManager, specs, site: float = 
 
     return actions, dims
 
+
+def crra(payoff: np.array, gamma: float = 1.0, p: List[float] = None) -> Tuple[float]:
+    """
+    Von Neumann-Morgenstern constant relative risk aversion (CRRA).
+    Returns the expected utility, certainty equivalent, expected value and the 
+    risk premium assocaiated to a management decision, based on an array of 
+    payoffs.
+    
+    
+    
+    Parameters
+    ----------
+    payoff : numpy.array
+        The payoff array includes all possible outcomes of the decision.
+    gamma : float
+        Risk aversion factor. If higher, the decision maker is willing to take more risk.
+    p : List[float]
+        List if probabilities associated to the payoffs. If None (default), all
+        payoffs are euqally probable.
+    
+    Returns
+    -------
+    eu : float
+        Expected utility (E[U(x)]) is the probability weighted sum of utilities associated with each possible payoff.
+    ce : float
+        certainty equivalent (CER) is the sure amount of payoff that is as desirable to the 
+        decision-maker (same utility) as the risky outcome.
+    ev : float
+        Expected value (EV)
+    rp : float
+        Risk premium (RP) sure amount of payoff that one is willing to forego at
+        maximum to reach a situation of certainty
+
+    Notes
+    -----
+    Translation of the R function `CRRA.leila` from taken from R-code Laila (Appendix 8, Master Thesis, 2020)
+    """
+    idx = payoff != 0.0
+    payoff = payoff[idx]
+    if p is not None:
+        p = p[idx]
+    
+    if abs(gamma - 1.0) < 1e-15:
+        # expected utility (E[U(x)]) is the probability weighted sum of utilities associated with each possible payoff
+        eu = np.average(np.log(payoff), weights=p)
+
+        # = input for uncertainty valuation
+        ce = np.exp(eu)
+        
+    else:
+        # expected utility, see eq. 3.2/3.3 (p. 44)
+        eu = np.average((np.power(payoff, 1 - gamma) - 1) / (1 - gamma), weights=p)  
+
+        # certainty equivalent, see eq. 3.4 (p. 44)
+        ce = np.power(eu * (1 - gamma) + 1, 1 / (1 - gamma))
+    
+    # expected value
+    ev = np.average(payoff, weights=p)
+    
+    # risk premium
+    rp = ev - ce
+
+    return (eu, ce, ev, rp)
+
+
+def atk(ce: Union[np.array, float], alpha: float = 0.75) -> Tuple[float]:
+    """
+    Atkinson's inequality aversion (ATK).
+
+    Parameters
+    ----------
+    ce : numpy.array
+        possible payoff vector, usually the certainty equivalent (CER) of the payoff vector
+    alpha : float
+        Uncertainty aversion coefficient. If higher, the decision maker is more risk averse.
+
+    Returns
+    -------
+    u : float
+        Utility (U(x)) is the utility associated with the payoff vector.
+    up : float
+        Uncertainty premium (UP) is the average payoff that the decision-maker would demand
+        in order to willingly take on uncertainty
+
+    Notes
+    -----
+    Translation of the R function `Atk` from taken from R-code Laila (Appendix 8, Master Thesis, 2020)
+    """
+    # get the number of scenarios
+    if isinstance(ce, float):
+        n = 1
+    else:
+        n = len(ce)
+
+    # get utility
+    if abs(alpha - 1.0) < 1e-15:
+        u = np.power(np.prod(ce), 1 / n)
+    else:
+        u = np.power((1 / n) * np.sum(np.power(ce, 1 - alpha)), 1 / (1 - alpha))
+
+    # uncertainty premium
+    up = ((1 / n) * np.sum(ce)) - u
+
+    return (u, up)
+
+
+def create_action_grid(dataManager: DataManager, resolution: float = 0.1, filter_: dict = {}) -> Tuple[list, list]:
+    """
+    Create a grid of management actions by balacing all three turbine types.
+    The function balances the area occupied by each turbine type in steps of
+    resolution. The grid is created for all possible combinations, wich sum
+    up to 100% of the area.
+
+    Parameters
+    ----------
+    dataManager : DataManager
+        RUINS data manager instance to load the wind provisioning dataset.
+    resolution : float, optional
+        Resolution of the grid. The default is 0.1. Has to be bewteen 0 and 1.
+    filter_ : dict, optional
+        Filter to apply to the wind provisioning dataset. The default is no
+        filter. Possible to load only joint datasets, fitler by GCM, RCM, RCP
+        or slice to years
+    
+    Returns
+    -------
+    actions : List[pandas.DataFrame]
+        Returns a dataframe for each management action. 
+        Each dataframe represents the provisioned windpower for each turbine type
+        and year.
+    scenarios : List[Tuples[float, float, float]]
+        Returns tuple for each management option. The tuple contains the
+        area shares for each of the turbines associated with the action.
+
+    """
+    # hardode the turbines
+    turbines = ['e53', 'e115', 'e126']
+    n_turbines = len(turbines)
+    
+    # get all combinations
+    gen = [np.arange(0.0, 1.0, resolution) for _ in range(n_turbines)]
+    scenarios = [t for t in product(*gen) if abs(sum(t) - 1.0) < 1e-5][1:]
+
+    # get the actions
+    actions, _ = windpower_actions_projection(dataManager, scenarios, filter_=filter_)
+
+    return actions, scenarios
+
+
+def uncertainty_analysis(actions: List[pd.DataFrame], gamma: float = 1.2, alpha: float = 0.75) -> pd.DataFrame:
+    """
+    Run the uncertainty analysis for a list of provisioned windpower scenarios. Each 
+    scenario represent the outcome of a management option.
+
+    Parameters
+    ----------
+    actions : List[pandas.DataFrame]
+        List of provisioned windpower scenarios. Each scenario represents the outcome
+        of a management option.
+    gamma : float, optional
+        Risk aversion factor. If higher, the decision maker is willing to take more risk.
+        The default is 1.2.
+    alpha : float, optional
+        Uncertainty aversion coefficient. If higher, the decision maker is more risk averse.
+        The default is 0.75.
+    
+    Returns
+    -------
+    df : pandas.DataFrame
+        Returns a dataframe with the uncertainty analysis results for each scenario.
+    
+    See Also
+    --------
+    ruins.processing.windpower.crra
+    ruins.processing.windpower.atk
+
+    """
+    result = defaultdict(lambda: [])
+
+    for action in actions:
+        # apply the KDE to obtain probability estimates
+        y = action.sum(axis=1).values
+
+        # resolve the annual windpower production to 100 steps
+        x = np.linspace(y.min(), y.max(), 100)
+
+        # get the KDE
+        kde = gaussian_kde(y)(x)
+
+        # apply constant relative risk aversion
+        eu, ce, ev, rp = crra(x, gamma=gamma, p=kde)
+    
+        # atkinson risk premium
+        au, up = atk(ce, alpha=alpha)
+
+        # append the results
+        result['eu'].append(eu)
+        result['ce'].append(ce)
+        result['ev'].append(ev)
+        result['rp'].append(rp)
+        result['au'].append(au)
+        result['up'].append(up)
+    
+    # return
+    return pd.DataFrame(result)
