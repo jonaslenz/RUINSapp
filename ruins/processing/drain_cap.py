212c0bc7823d0569751808a572106ea97206bb33
Mirko Mälicke <mirko@maelicke-online.de>
2022-08-03 13:20:46 +0200

    using numpy formatted docstrings

--- ruins/processing/drain_cap.py
+++ ruins/processing/drain_cap.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 import pandas as pd
 
@@ -5,25 +6,43 @@ x = np.array([7.,6.,5.,4.,3.5,3.,2,1,0,5.,4.,3.5,3.,2])
 y = np.array([0,4.2,8.4,12.6,14.5,15.8,17.5,19,20.5,8.4,12.6,14.5,15.8,17.5])
 pumpcap_fit = np.polynomial.polynomial.Polynomial.fit(x = x, y = y, deg = 2)
 
-#### definition of drainage capacity function - it compares the drainage capacity from pumps with that from channels 
-def drain_cap (h_tide, h_store, h_min = -2000, pump_par = pumpcap_fit, channel_par = [1.016 , 2572.], dh = 50, wind_safe = 0, gradmax = 4000):
-    ''' This function tries to find the maximal flow rate in a system with a pump and a canal.
+ 
+def drain_cap(h_tide: np.ndarray, h_store: np.ndarray, h_min: int = -2000, pump_par = pumpcap_fit, channel_par: Tuple[float, float] = [1.016 , 2572.], dh: int = 50, wind_safe: int = 0, gradmax: int = 4000):
+    """
+    Find sthe maximal flow rate in a system with a pump and a canal.
     The flow through the pump is determined by the gradient from inner to outer water level and a pump function.
     The flow through the canal is determined by the gradient from the canal water level to the pump inner water level and a flow function.
     The inner water level is unknown and estimated within this function, but a lower limit can be set.
     
-    h_tide :: the outer water level at the pump
-    h_store :: the canal water level
-    h_min :: the lower boundary of the inner water level
-    pump_par :: parameters of the pump function
-    channel_par :: parameters of the canal flow function
-    dh :: increment of inner water level estimation
-    wind_safe :: gradient from canal to inner water level, which is induced by wind and therefore not contributing to flow
-    gradmax :: maximum gradient from inner to outer water level, at which pumps shall run 
+    Parameters
+    ----------
+    h_tide : np.ndarray
+        the outer water level at the pump
+    h_store : np.ndarray
+         the canal water level
+    h_min : int
+        the lower boundary of the inner water level
+    pump_par : np.ndarray
+        parameters of the pump function
+    channel_par : Tuple[float, float]
+        parameters of the canal flow function
+    dh : int
+        increment of inner water level estimation
+    wind_safe : int
+        gradient from canal to inner water level, which is induced by wind and therefore not contributing to flow
+    gradmax :int
+        maximum gradient from inner to outer water level, at which pumps shall run 
 
-    returns an array with [0] the actual maximum flow through canal and pump, [1] the estimated inner water level and [2] the maximum flow, which could be pumped if not limited by canals
-    '''
+    Returns
+    -------
+    a_channel : np.ndarry
+        the actual maximum flow through canal and pump
+    h_min : float
+        the estimated inner water level
+    q_pump : float
+         the maximum flow, which could be pumped if not limited by canals
 
+    """
     # set h_min to either absolute technical lower limit or maximal pump gradient 
     h_min = np.maximum(h_min, h_tide - gradmax)
     
@@ -43,10 +62,6 @@ def drain_cap (h_tide, h_store, h_min = -2000, pump_par = pumpcap_fit, channel_p
         else:
             q_channel = ((((h_store - h_min) - wind_safe)**channel_par[0])/channel_par[1])
             
-#        print("q_pump: "+str(q_pump))
-#        print("q_channel: "+str(q_channel))
-#        print("h_min: "+str(h_min))
-
         # end the loop, if h_min is set to value that q_channel is smaller than q_pump, otherwise increase h_min
         if(q_pump >= q_channel or q_channel <= 0) :
             pumplim = False
@@ -56,10 +71,12 @@ def drain_cap (h_tide, h_store, h_min = -2000, pump_par = pumpcap_fit, channel_p
     # set output h_min to either technical lower limit or water level in canals
     h_min = np.minimum(h_min, h_store)
 
-    return (q_channel,h_min, q_pump)
+    return (q_channel, h_min, q_pump)
 
 def storage_model (x, z, storage = 0, h_store = -1400, canal_area = 4, advance_pump = 0, maxdh = 6000):
-    
+    """
+    Storage model used for the Krummhörn region
+    """
     store = []
     h_min = []
     q_pump = []
